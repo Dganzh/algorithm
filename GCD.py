9077e5dbc858cdb6741edc33fd97f03ea501c34d
Dganzh <dganzh@163.com>
2016-10-05 20:13:36 +0800

    add GCD、primes

---
+++ GCD.py
@@ -0,0 +1,39 @@
+
+
+def gcd(a,b):
+    '''
+    Greatest Common Divisor
+    辗转相除法求 a和b的最大公约数
+    若|a|<|b|则a%b=a
+    即小数对大数（指绝对值大小）取余仍是小数
+    '''
+    while b!=0:
+        a,b=b,a%b
+    return a
+#print(gcd(319,377))  #29
+
+#构造序列(只需奇数即可)
+def _odd_iter():
+    n=1
+    while True:
+        n+=2
+        yield n
+#定义筛选函数
+def _not_divisible(n):
+    return lambda x:x%n>0
+
+def primes():
+    yield 2
+    it=_odd_iter()
+    while True:
+        n=next(it)
+        yield n
+        it=filter(_not_divisible(n),it)
+
+
+for n in primes():
+    if n<100:
+        print(n)
+    else:
+        break
+
--- readme.txt
+++ readme.txt
@@ -1,4 +1,25 @@
 Git is a version control system.
 Git is free software.
 
-什么鬼、。？本地怎么能看到啊？
+在github上修改后，若要把修改合并到本地，则需：
+$ git fetch origin
+$ git merge origin/master
+
+创建本地git仓库,在文件中：
+$ git init
+添加文件到本地仓库：
+$ git add file1.doc file2.txt file3.py
+提交：
+$ git commit -m "this is why to change"
+把远程仓库与本地仓库关联：
+$ git remote add origin https://github.com/Dganzh/algorithm-problem.git
+把本地仓库内容推送到远程仓库：
+$ git push -u origin master   #首次推送要加-u
+
+查看远程仓库有哪些：
+$ git remote [-v]
+删除远程仓库(名为origin)：
+$ git remote rm origin
+
+
+
